f499efd666d9ed7ca32e18438541e6c1a49d0c57
Ian Birle <IBirle@umass.edu>
2019-11-13 19:02:19 -0500

    Completed most of the Employee Page.

---
+++ front_end/assets/js/employees.js
@@ -0,0 +1,22 @@
+
+$(document).ready( function () {
+    var buttonHTML = "                      <div class=\"td-actions text-right\">\n                          <button type=\"button\" rel=\"tooltip\" class=\"btn btn-info trainingButton\" data-toggle=\"modal\" data-target=\"#trainingModal\">\n                              <i class=\"material-icons\">work</i>\n                          </button>\n                         <button type=\"button\" rel=\"tooltip\" class=\"btn btn-danger\" data-toggle=\"modal\" data-target=\"#deleteWarningModal\">\n                              <i class=\"material-icons\">close</i>\n                          </button>\n                      </div>";
+    $('#employeeTable').dataTable( {
+
+        "ajax": "testData/employeeData.txt",
+
+        "columnDefs": [ {
+            "targets": -1,
+            "data": null,
+            "orderable" : false,
+            "className": "td-actions text-center",
+            "defaultContent": buttonHTML
+        } ]
+    } );
+
+    $('#trainingTable').dataTable({
+        "ajax": "testData/hold.txt"
+    });
+
+} );
+
