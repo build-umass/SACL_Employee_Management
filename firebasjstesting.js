f23b3d6d8b4c4b805222f7fa222e27b74408f1ec
Emmie Ohnuki <emaohn@gmail.com>
2019-11-13 19:52:01 -0500

    getting data testing

--- firebasjstesting.js
+++ firebasjstesting.js
@@ -1,5 +1,6 @@
 console.log("HIIII");
 
+//writing information
 function writeUserData(userId, name, email) {
     firebase.database().ref('users/' + userId).set({
         username: name,
@@ -7,10 +8,24 @@ function writeUserData(userId, name, email) {
     });
 }
 
+function getUserData(userID) {
+    var ref = firebase.database().ref('users/' + userID)
+    ref.on("value", function(snapshot) {
+        console.log("User ID: " + userID + );
+    })
+}
+
 writeUserData("aksduhska", "emmie", "eohnuki@umass.edu");
 writeUserData("gd7823ejh", "bob", "blahblah@umass.edu");
 writeUserData("89786tydghs", "tom", "hello@umass.edu");
 writeUserData("87y3eh=ka", "bill", "umass@umass.edu");
+
+//getting information
+var dbref = firebase.database().ref('users/');
+dbref.on("value", function(snapshot) {
+    console.log(snapshot.val());
+})
+
 
 
 
