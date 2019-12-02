04747ea25a02c223dc7fa12b7415ca7d2d2e2fd2
Emmie Ohnuki <emaohn@gmail.com>
2019-12-02 14:37:27 -0500

    mail working from gmail

---
+++ mailtesting.js
@@ -0,0 +1,26 @@
+'use strict';
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'emaohn@gmail.com',
+    pass: 'epgwbbnnnrjgtzcd'
+  }
+});
+
+var mailOptions = {
+  from: 'emaohn@gmail.com',
+  to: 'emaohn@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+
